6d27f529e5201be638d5f3ad0ede712c8aa47355
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2023-11-28 20:55:46 +0200

    ../working on login

--- login.py
+++ login.py
@@ -3,6 +3,7 @@ def login():
     ask_username = str(input('Username'))
     ask_password = str(input('Password'))
 
+login()
 #--------------------------------------------------------------------------------------------
 
 #str stands for "string." It's a data type used to represent text as a sequence of characters
