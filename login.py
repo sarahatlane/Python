3d1610655712defd8a693dbab504ed66423b3237
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2023-11-28 20:28:34 +0200

    ../working on login

---
+++ login.py
@@ -0,0 +1,6 @@
+#function (def is a keyword used to define functions)
+def login():
+    ask_username = str
+
+#--------------------------------------------------------------------------------------------
+#str stands for "string." It's a data type used to represent text as a sequence of characters
