1229a5324411883661b65e6d17a5d355c2576467
sarahatlane <61564682+sarahatlane@users.noreply.github.com>
2023-11-29 20:41:23 +0200

    ../working on login

--- login2.py
+++ login2.py
@@ -1,14 +1,7 @@
-import getpass  
-  
+def register():
+    username = input("Enter a username: ")
+    password = input("Enter a password: ")
 
-users = {'john': 'password123', 'jane': 'password456', 'bob': 'password789'}  
-
-  
-username = input('Enter username: ')  
-password = getpass.getpass('Enter password: ')  
-
-  
-if username in users and users[username] == password:  
-    print('Access granted')  
-else:  
-    print('Access denied')  
+    with open("users.txt", "a") as file:
+        file.write(f"{username}:{password}\n")
+    print("Registration successful!")
